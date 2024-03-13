6ddf70ebc8094a2fe41de4d6a5990ab6d51bcd2e
nicolas <ntraut9@gmail.com>
2024-03-13 17:13:03 +0100

    add new tests

--- test.mjs
+++ test.mjs
@@ -1,4 +1,6 @@
+/* eslint-disable max-lines */
 import assert from 'assert';
+import { text, buffer } from 'node:stream/consumers';
 
 import { chromium, firefox, webkit } from '@playwright/test';
 
@@ -27,6 +29,10 @@ describe('Test Thresholdmann', () => {
         await page.goto('http://127.0.0.1:8080');
       });
 
+      after(async () => {
+        await browser.close();
+      });
+
       describe('Unit tests', () => {
         describe('Trivial test', () => {
           it('should say hey', async () => {
@@ -44,18 +50,42 @@ describe('Test Thresholdmann', () => {
           });
           it('should display "Choose..." message', async () => {
             const msg = await page.evaluate(() => document.querySelector('.box_input').innerText);
-            assert.strictEqual(msg, '\nChoose a .nii.gz or a .nii file or drag it here.');
+            assert.strictEqual(msg.trim(), 'Choose a .nii.gz or a .nii file or drag it here.');
           });
           it('init with test nifti file', async () => {
             const pathString = './img/bear_uchar.nii.gz';
-            sdim = await page.evaluate(async (path) => {
-              await window.initWithPath(path);
-
-              return window.globals.mv.mri.s2v.sdim;
-            }, pathString);
+            const fileChooserPromise = page.waitForEvent('filechooser');
+            await page.click('#loadNifti');
+            const fileChooser = await fileChooserPromise;
+            await fileChooser.setFiles(pathString);
+            await page.waitForSelector('.viewer');
+            sdim = await page.evaluate(() => window.globals.mv.mri.s2v.sdim);
             assert.ok(Array.isArray(sdim));
             assert.strictEqual(sdim.length, 3);
-          }).timeout(5000);
+          }).timeout(10000);
+        });
+
+        describe('can change slice', () => {
+          it('check initial slice', async () => {
+            const initialSlice = sdim[0] / 2 | 0;
+            const slice = await page.evaluate(() => (
+              window.globals.mv.views[0].slice
+            ));
+            assert.strictEqual(slice, initialSlice);
+          });
+
+          it('can change to another slice', async () => {
+            const targetSlice = sdim[0] * 3 / 4 | 0;
+            await page.evaluate((newSlice) => {
+              const slider = document.querySelector('input.slice');
+              slider.value = newSlice;
+              slider.dispatchEvent(new Event('input', { bubbles: true }));
+            }, targetSlice);
+            const slice = await page.evaluate(() => (
+              window.globals.mv.views[0].slice
+            ));
+            assert.strictEqual(slice, targetSlice);
+          });
         });
 
         describe('can switch planes', () => {
@@ -140,13 +170,206 @@ describe('Test Thresholdmann', () => {
             assert.ok(points[0][1] < points[1][1]);
             assert.ok(points[0][2] > points[1][2]);
           });
+          it('when selecting the first point, the first threshold slider is selected', async () => {
+            await page.click('#tools .mui[title="Select"]');
+            const clickX = canvasBoundingBox.x + canvasBoundingBox.width / 2;
+            const clickY = canvasBoundingBox.y + canvasBoundingBox.height / 2;
+            await page.mouse.click(clickX, clickY);
+            const firstSliderSelected = await page.$eval('#control-table tbody tr:nth-child(1)', (row) => row.classList.contains('selected'));
+            const secondSliderSelected = await page.$eval('#control-table tbody tr:nth-child(2)', (row) => row.classList.contains('selected'));
+            assert.strictEqual(firstSliderSelected, true);
+            assert.strictEqual(secondSliderSelected, false);
+          });
+          it('when selecting the second point, the second threshold slider is selected', async () => {
+            await page.click('#tools .mui[title="Select"]');
+            const clickX = canvasBoundingBox.x + canvasBoundingBox.width * 2 / 3;
+            const clickY = canvasBoundingBox.y + canvasBoundingBox.height * 2 / 3;
+            await page.mouse.click(clickX, clickY);
+            const firstSliderSelected = await page.$eval('#control-table tbody tr:nth-child(1)', (row) => row.classList.contains('selected'));
+            const secondSliderSelected = await page.$eval('#control-table tbody tr:nth-child(2)', (row) => row.classList.contains('selected'));
+            assert.strictEqual(firstSliderSelected, false);
+            assert.strictEqual(secondSliderSelected, true);
+          });
+          it('when selecting the first threshold slider, the first point slice is displayed', async () => {
+            await page.click('.axi-btn');
+            await page.click('#control-table tbody tr:nth-child(1)  td:first-child');
+            const sliderValue = await page.$eval('input.slice', (slider) => slider.value);
+            const slice = await page.evaluate(() => (window.globals.mv.views[0].slice));
+            assert.strictEqual(parseInt(sliderValue, 10), points[0][2]);
+            assert.strictEqual(slice, points[0][2]);
+          });
+          it('when selecting the second threshold slider, the second point slice is displayed', async () => {
+            await page.click('#control-table tbody tr:nth-child(2)  td:first-child');
+            const sliderValue = await page.$eval('input.slice', (slider) => slider.value);
+            const slice = await page.evaluate(() => (window.globals.mv.views[0].slice));
+            assert.strictEqual(parseInt(sliderValue, 10), points[1][2]);
+            assert.strictEqual(slice, points[1][2]);
+          });
           it('can remove a point', async () => {
+            await page.click('.sag-btn');
             await page.click('#tools .mui[title="Remove"]');
             const clickX = canvasBoundingBox.x + canvasBoundingBox.width * 2 / 3;
             const clickY = canvasBoundingBox.y + canvasBoundingBox.height * 2 / 3;
             await page.mouse.click(clickX, clickY);
             points = await page.evaluate(() => window.globals.points);
             assert.strictEqual(points.length, 1);
+          });
+        });
+
+        describe('can adjust threshold', () => {
+          it('check initial threshold', async () => {
+            const initThreshold = await page.evaluate(() => (
+              window.globals.values[0]
+            ));
+            assert.strictEqual(initThreshold, 127);
+          });
+
+          it('can adjust threshold with slider', async () => {
+            const targetThreshold = 75;
+            const sliderElement = await page.$('#control-table tbody tr:first-child td.slider-val input');
+            await page.evaluate(({slider, value}) => {
+              slider.value = value;
+              slider.dispatchEvent(new Event('input', { bubbles: true }));
+            }, {slider: sliderElement, value: targetThreshold.toString()});
+            const newThreshold = await page.evaluate(() => (
+              window.globals.values[0]
+            ));
+            assert.strictEqual(newThreshold, targetThreshold);
+          });
+
+          it('can adjust threshold with input', async () => {
+            const targetThreshold = 125;
+            const inputElement = await page.$('#control-table tbody tr:first-child td.text-val input');
+            await inputElement.fill(String(targetThreshold));
+            await inputElement.dispatchEvent('change');
+            const newThreshold = await page.evaluate(() => (
+              window.globals.values[0]
+            ));
+            assert.strictEqual(newThreshold, targetThreshold);
+          });
+        });
+
+        describe('can adjust opacity, contrast and brightness', () => {
+          it('check initial values', async () => {
+            const {opacity, contrast, brightness} = await page.evaluate(() => ({
+              opacity: window.globals.alpha,
+              contrast: window.globals.contrast,
+              brightness: window.globals.brightness
+            }));
+
+            assert.strictEqual(opacity, 0.5);
+            assert.strictEqual(contrast, 1);
+            assert.strictEqual(brightness, 1);
+          });
+
+          it('can adjust opacity', async () => {
+            const targetOpacity = 40;
+            await page.evaluate((opacity) => {
+              const alphaSlider = document.querySelector('#image-adjust input[type="range"][oninput="changeAlpha(event)"]');
+              alphaSlider.value = opacity;
+              alphaSlider.dispatchEvent(new Event('input', { bubbles: true }));
+            }, targetOpacity);
+            const newOpacity = await page.evaluate(() => (
+              window.globals.alpha
+            ));
+            assert.strictEqual(newOpacity, targetOpacity / 100);
+          });
+
+          it('can adjust contrast', async () => {
+            const targetContrast = 80;
+            await page.evaluate((contrast) => {
+              const contrastSlider = document.querySelector('#image-adjust input[type="range"][oninput="changeContrast(event)"]');
+              contrastSlider.value = contrast;
+              contrastSlider.dispatchEvent(new Event('input', { bubbles: true }));
+            }, targetContrast);
+            const newContrast = await page.evaluate(() => (
+              window.globals.contrast
+            ));
+            assert.strictEqual(newContrast, targetContrast / 100);
+          });
+
+          it('can adjust brightness', async () => {
+            const targetBrightness = 120;
+            await page.evaluate((brightness) => {
+              const brightnessSlider = document.querySelector('#image-adjust input[type="range"][oninput="changeBrightness(event)"]');
+              brightnessSlider.value = brightness;
+              brightnessSlider.dispatchEvent(new Event('input', { bubbles: true }));
+            }, targetBrightness);
+            const newBrightness = await page.evaluate(() => (
+              window.globals.brightness
+            ));
+            assert.strictEqual(newBrightness, targetBrightness / 100);
+          });
+
+        });
+
+        describe('3D render', () => {
+          let newPage;
+
+          it('can open the 3D volume view window', async () => {
+            const newPagePromise = page.context().waitForEvent('page');
+            await page.click('#render3D');
+            newPage = await newPagePromise;
+            const newPageUrl = newPage.url();
+            assert.strictEqual(newPageUrl, 'http://127.0.0.1:8080/render3D/index.html');
+          });
+
+        //   after(async () => {
+        //     if (newPage) {
+        //       await newPage.close();
+        //     }
+        //   });
+        });
+
+        describe('Saving', () => {
+          it('Save Control Points', async () => {
+            page.on('dialog', async (dialog) => {
+              if (dialog.type() === 'prompt') {
+                await dialog.accept();
+              }
+            });
+            const downloadPromise = page.waitForEvent('download');
+            await page.click('#saveControlPoints');
+            const download = await downloadPromise;
+            const readStream = await download.createReadStream();
+            const downloadedJson = await text(readStream);
+            const controlPoints = JSON.parse(downloadedJson);
+            assert.deepStrictEqual(controlPoints, { points: [sdim.map((element) => element / 2 | 0)], values: [125] });
+          });
+          it('Save Mask', async () => {
+            page.on('dialog', async (dialog) => {
+              if (dialog.type() === 'prompt') {
+                await dialog.accept();
+              }
+            });
+            const downloadPromise = page.waitForEvent('download');
+            await page.click('#saveMask');
+            const download = await downloadPromise;
+            const readStream = await download.createReadStream();
+            const niftiBuffer = await buffer(readStream);
+
+            // check that dowloaded nifti is a gzip file
+            assert.deepStrictEqual(niftiBuffer.slice(0, 2), Buffer.from([0x1F, 0x8B]));
+          }).timeout(5000);
+        });
+
+        describe('Loading', () => {
+          it('Load Control Points', async () => {
+            const fileChooserPromise = page.waitForEvent('filechooser');
+            await page.click('#loadControlPoints');
+            const fileChooser = await fileChooserPromise;
+            await fileChooser.setFiles({
+              name: 'control-points.json',
+              mimeType: 'application/json',
+              buffer: Buffer.from(JSON.stringify({ points: [[50, 50, 50]], values: [100]}), 'utf-8')
+            });
+            // wait for the new points to be loaded
+            await page.waitForTimeout(10);
+            const points = await page.evaluate(() => window.globals.points);
+            const threshold = await page.evaluate(() => window.globals.values[0]);
+            assert.strictEqual(points.length, 1);
+            assert.deepStrictEqual(points, [[50, 50, 50]]);
+            assert.strictEqual(threshold, 100);
           });
         });
       });
